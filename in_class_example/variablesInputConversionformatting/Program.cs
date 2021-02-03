da08c4246d294ba44cf33515e48a72a8a873ba70
Nicole Goral <Nicole.A.Goral-1@ou.edu>
2021-02-02 18:18:32 -0600

    Creaed a new solution for in class examples as well as a project for variables, conversions, and formatting.

---
+++ in_class_example/variablesInputConversionformatting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace variablesInputConversionformatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
